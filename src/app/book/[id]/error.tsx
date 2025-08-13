f59eb73620682a0bc15a8785b64df74d782ee84d
HYeDdDiLaNd <anotherHyedddi@gmail.com>
2025-08-13 20:27:50 +0900

    백앤드 서버까지 다시 리프레시하도록 처리

--- src/app/book/[id]/error.tsx
+++ src/app/book/[id]/error.tsx
@@ -22,9 +22,10 @@ export default function Error({
         type="button"
         onClick={() => {
           startTransition(() => {
+            /* startTransition: 하나의 콜백함수를 인수로 전달받아서 이 콜백함수 안에 들어있는 ui를 변경시키는 작업을 모두 일괄적으로 처리해줌, */
             router.refresh(); /* 현재 페이지에 필요한 서버컴포넌트들을 다시 불러옴: 비동기임  */
             reset(); //에러 상태 초기화, 컴포넌트들 다시 렌더링
-          });
+          }); //넥스트에서는 레이아웃을 살리기 위해 어쩔 수 없이 error.tsx를 경로별로 만들어줘야하는 경우가 생길 수도 있다.
         }}
       >
         {/* reset(): 서버로 부터 받은 데이터를 가지고 클라이언트 컴포넌트들을 다시 그림. */}
